bb2729059b45428c5e0d4ad000b4d83437187cbb
Enrique Gomez <egomezmo@prodigy.net.mx>
2019-02-15 10:35:10 -0600

    all inside

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# testings-forms
+# Annual Budget
+
+##OBJECTIVE. Integrate all the knowledge acquire so far to start doing tables, formulas and graphs in order to analyze information.
+
+Develop the use of forms, database and graphics tools in React.
+
+The App contains folders as it shows:
+
+###TIPS - General information about get in a good shape in personal finances.
+###TOOL - Form with inputs to keep track of a general income and expenses and see a graph, that changes in real time, of budget in a yearly example.
+
+FUTURE DESIRABLE CHARACTERISTICS:
+* Develop a detail table of incomes and expenses.
+* Take the table to a pdf format. 
+
+###MAIN TECHNOLOGIES:
+* Node an Express Web server. Is a minimal and flexible Node.js web application framework that provides a robust set of features to develop web and mobile applications.
+* React. A JavaScript library for building user interfaces
+* Mongo Db. MongoDB’s flexible document data model makes working with data intuitive, whether you’re building an app from scratch or updating an existing one.
+* Victory. React.js components for modular charting and data visualization.
+* Git Hub. The world's leading software development platform.
+* Heroku. It is a cloud platform as a service (PaaS) that lets companies build, deliver, monitor and scale apps.
+
+
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,40 +1,36 @@
 import React, { Component } from "react";
-// import DeleteBtn from "../components/DeleteBtn";
-// import Jumbotron from "../components/Jumbotron";
-// import API from "../utils/API";
-// import { Link } from "react-router-dom";
-// import { Col, Row, Container } from "../components/Grid";
-// import { List, ListItem } from "../components/List";
-// import { Input, TextArea, FormBtn } from "../components/Form";
+import "./Main.css";
+
 
 class Books extends Component {
 
   render() {
     return (
       <div className="container">
-        <br/><br /><br/>
-        <h1>Register</h1>
-        <form>
+        <br /><br /><br />
+        <h2>Register or start your Session</h2>
 
-          <div className="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-          </div>
+        <div id="register">
+          <form>
+            <div className="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+              <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
 
-          <div className="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-          </div>
-          <div className="form-group form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-            <label className="form-check-label" for="exampleCheck1">Check me out</label>
-          </div>
-          <button type="submit" class="btn btn-primary">Submit</button>
-
-
-
-        </form>
+            <div className="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+            </div>
+            <div className="form-group form-check">
+              <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+              <label className="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            <hr/>
+            <h3>Keys to success inside!!</h3>
+          </form>
+        </div>
 
       </div>
     );
